79307612685ce3543ba49ea6edb75c553e270cfa
Jacobtread <jacobtread@gmail.com>
2023-03-25 12:45:44 +1300

    Added extra meta and icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jacobtread | Full-Stack Developer';
-export const SITE_DESCRIPTION = 'This website is my personal / portfolio website. It contains information about me, my contact information, and a list of projects.';
+export const SITE_TITLE = "Jacobtread | Full-Stack Developer";
+export const SITE_DESCRIPTION = "This website is my personal / portfolio website. It contains information about me, my contact information, and a list of projects.";
+export const SITE_KEYWORDS = "developer,jacobtread,jacob,programming,software,fullstack,web";
