34c51f58a90ec4ae83947f918c30be8f6060ddf6
Jacobtread <jacobtread@gmail.com>
2023-03-24 16:01:55 +1300

    Removed technology section and other unuseds

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jacobtread | Full-Stack Developer';
+export const SITE_DESCRIPTION = 'This website is my personal / portfolio website. It contains information about me, my contact information, and a list of projects.';
