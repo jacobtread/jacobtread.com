ed76a79b049a222b219d38f8146b99d55e19adda
Jacobtread <jacobtread@gmail.com>
2023-03-26 13:03:35 +1300

    Fix url for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jacobtread.com",
+  site: "https://jacobtread-astro.pages.dev",
   integrations: [mdx(), sitemap(), svelte()],
   markdown: {
     shikiConfig: {
