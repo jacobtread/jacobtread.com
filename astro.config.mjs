3f939d02b34e8496949b31a65ef5d4f84b2efbca
Jacobtread <jacobtread@gmail.com>
2024-01-16 17:00:02 +1300

    fix: remove leftover svelte files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobtread.com",
-  integrations: [mdx(), sitemap(), svelte()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
