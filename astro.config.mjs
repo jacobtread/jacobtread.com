2ab3537d03cca374df52000da5da862c7c6cb5be
Jacobtread <jacobtread@gmail.com>
2025-12-01 12:56:26 +1300

    feat: update dependencies and use astro-shield fork
    Switched @kindspells/astro-shield for @jacobtread/astro-shield which is
    updated for the latest astro version and republished

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import { shield } from '@kindspells/astro-shield'
+import { shield } from '@jacobtread/astro-shield';
 
 // https://astro.build/config
 export default defineConfig({
